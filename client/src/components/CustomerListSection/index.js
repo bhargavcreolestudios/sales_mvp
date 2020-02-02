88cff8119e97f96b665f8b052f0b4acbb364e668
bhargav <bhargav@creolestudios.com>
2020-02-02 11:57:45 +0530

    changes in the listing name service

--- client/src/components/CustomerListSection/index.js
+++ client/src/components/CustomerListSection/index.js
@@ -20,8 +20,7 @@ class CustomerListSection extends React.Component {
     this.setState({ moreOption: true, key: key });
   };
   async componentDidMount() {
-    const { customers } = this.state;
-    let allCustomers = await customerService.getAll();
+    let allCustomers = await customerService.getCustomers();
     this.setState({ customers: allCustomers });
   }
   PopoverAction = (id, e) => {
@@ -117,107 +116,6 @@ class CustomerListSection extends React.Component {
         dataIndex: "fax",
         key: "fax",
         render: text => <p className="content otherContent">{text}</p>
-      }
-    ];
-    const data = [
-      {
-        key: 1,
-        firstName: "A & G Sales",
-        displayName: "A & G Sales Fence & Supply",
-        billingAddress: "11926 Woodruff Ave.",
-        city: "Downey, ",
-        state: "CA 90241",
-        customerType: "[ASSIGNED]",
-        phone: "(562) 803-1888",
-        fax: "(562) 803-1888"
-      },
-      {
-        key: 2,
-        firstName: "Able Crane Service",
-        displayName: "Able Crane Service",
-        billingAddress: "P.O. Box 2806",
-        city: "Santa Fe Springs,",
-        state: " CA 90670",
-        customerType: "Rental Equipment",
-        phone: "(562) 946-1796",
-        fax: "(626) 454-1602"
-      },
-      {
-        key: 3,
-        firstName: "AC Gates",
-        displayName: "ACFolding Gates",
-        billingAddress: "1374 E. Ninth St.",
-        city: "Pomona, ",
-        state: "CA 91766",
-        customerType: "Manufacturer",
-        phone: "(866) 944-2837",
-        fax: "(866) 796-4283"
-      },
-      {
-        key: 4,
-        firstName: "Academy",
-        displayName: "Academy Doors",
-        billingAddress: "555 Maitland Ave",
-        city: "Ontario, ",
-        state: "CA 91761",
-        customerType: "Manufacturer",
-        phone: "(909) 988-0517",
-        fax: "(909) 391-7024"
-      },
-      {
-        key: 5,
-        firstName: "ACME",
-        displayName: "ACME Home Elevator",
-        billingAddress: "4740 E. 2nd Street, Suite 200",
-        city: "Benicia, ",
-        state: "CA 94510",
-        customerType: "Subcontractor",
-        phone: "(562) 946-1796",
-        fax: "(626) 454-1602"
-      },
-      {
-        key: 6,
-        firstName: "Able Crane Service",
-        displayName: "Able Crane Service",
-        billingAddress: "P.O. Box 2806",
-        city: "Santa Fe Springs, ",
-        state: "CA 90670",
-        customerType: "Rental Equipment",
-        phone: "(562) 946-1796",
-        fax: "(626) 454-1602"
-      },
-      {
-        key: 7,
-        firstName: "AC Gates",
-        displayName: "ACFolding Gates",
-        billingAddress: "1374 E. Ninth St.",
-        city: "Pomona, ",
-        state: "CA 91766",
-        customerType: "Manufacturer",
-        phone: "(562) 946-1796",
-        fax: "(626) 454-1602"
-      },
-      {
-        key: 8,
-        firstName: "Academy",
-        displayName: "Academy Doors",
-        billingAddress: "555 Maitland Ave",
-        city: "Ontario, ",
-        state: "CA 91761",
-        customerType: "Manufacturer",
-        phone: "(562) 946-1796",
-        fax: "(626) 454-1602"
-      },
-      {
-        key: 9,
-        firstName: "ACME",
-        displayName: "ACME Home Elevator",
-        billingAddress: "4740 E. 2nd Street, Suite 200",
-        city: "Benicia, ",
-        state: "CA 94510",
-        customerType: "Subcontractor",
-        phone: "(562) 946-1796",
-        fax: "(626) 454-1602"
       }
     ];
     const rowSelection = {
